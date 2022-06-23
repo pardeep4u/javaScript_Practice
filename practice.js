107e3f4138fa6b4efd083cf3cafae61274744900
sourav4u <sourav.vashisht222@gmail.com>
2022-06-23 21:58:01 +0530

    New practice question are added

--- practice.js
+++ practice.js
@@ -8,7 +8,7 @@ Sample Output : Today is : Tuesday.
 Current time is : 10 PM : 30 : 38
 
 */
-
+/*
 
 var today = new Date();
 var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
@@ -34,7 +34,7 @@ console.log(window.print());
 Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched"
 
 */
-
+/*
 var ran = Math.random();
 ran = (ran * 10) + 1;
 ran = Math.floor(ran);
@@ -44,7 +44,7 @@ var inp = prompt("Enter any Number");
 inp == ran ? console.log("oh yes") : console.log("oh no");
 
 */
-
+/*
 var d = new Date(2022,0,2);
 console.log(d.getMonth(),d.getDate(),d.getDay());
 
@@ -54,7 +54,7 @@ console.log(d.getMonth(),d.getDate(),d.getDay());
 get document URL 
 
 */
-
+/*
 console.log(document.URL);
 
 var filename = "19bcs1896.pdf";
@@ -67,7 +67,7 @@ console.log(filename.split(".").pop());
 Write a JavaScript program to create a new string adding "Py" in front of a given string. If the given string begins with "Py" then return the original string.
 
 */
-
+/*
 var nick = "Pyring";
 var check = nick.substring(0,2);
 console.log(check);
@@ -94,10 +94,105 @@ console.log(arrr.join(""));
 
 
 /* new */
-
+/*
 var ss = "TESTING";
 var tt = "sourav";
 console.log( ss.slice(1)+tt.slice(1) );
 var last = tt.substring(tt.length,tt.length- 3);
 console.log(last + tt[2][4]);
+
+*/
+
+
+
+
+function Question(que,opt,corOption){
+    this.que = que;
+    this.opt = opt;
+    this.corOption = corOption;
+
+    return {
+        que,opt,corOption
+    }
+}
+
+
+var que1 = new Question("what is my name",{"1" : "sourav","2" : "pardeep"},1);
+var que2 = new Question("what is my career",{"1" : "SWD","2" : "WD"},2);
+var que3 = new Question("what is my future",{"1" : "Good","2" : "Bamb , execellent , awesome , No-stress"},2);
+
+var bundle = [que1,que2,que3];
+
+
+
+function printq(obj,num){
+        var ques = obj.que;
+        var option = obj.opt;
+       // var answer = obj.corOption;
+        console.log(num + " " + ques);
+        for (const property in option) {
+            console.log(`${property}: ${option[property]}`);
+          }
+}
+
+
+function game(bundle) {
+    var count = 1;
+    var score = 0;
+    var gameon = true;
+    
+    while(gameon){
+        var ran = Math.floor((Math.random() * 3) + 1);
+        
+        printq(bundle[ran],count);
+        count++;
+        var val = prompt("Enter the answer");
+        if(val === "exit"){
+        
+            console.log(" Thanks for playing");
+            gameon = false;
+    
+        }else{
+            val = parseInt(val);
+            if(val === bundle[ran].corOption){
+                console.log("correct option");
+                score++;
+                console.log("score is " + score);
+                console.log("-----------------------------------------");
+               
+            }
+            else{
+                console.log("Wrong answer");
+            }
+        }
+    }
+        
+
+
+    
+}
+
+
+
+game(bundle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
