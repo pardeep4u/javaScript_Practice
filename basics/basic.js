25d71459e9c7abd6b5872b6ae1562e77554a6d00
sourav4u <sourav.vashisht222@gmail.com>
2022-06-26 21:20:52 +0530

    New folder just added

---
+++ basics/basic.js
@@ -0,0 +1,87 @@
+// Data types 
+/*{
+    let x;
+    let y = null;
+    let array = [{"number":1.1,"array":false},{"string":"just simple plain text"},{"boolean":false},{"undefined":x},{"null":y}];
+    console.table(array);
+    console.log(typeof(x) );
+    console.log(typeof(y));
+
+
+    let c1 = 28;
+    let c2 = "28";
+/*
+    let i = prompt("Enter some value");
+    if(y===i){
+        console.log("we are in if block");
+    }else{
+        console.log("we are in else block");
+    }
+*/
+/*
+    if(c2===c1){
+        console.log("we are in if block");
+        console.log(typeof(c1));
+        console.log(typeof(c2));
+    }else{
+        console.log("we are in else block");
+    }
+
+    c1 > 25 ? console.log("yes it is greater than 25") : console.log("no it is not greater than 25");
+
+    /*
+
+    falsey values  : Nan , undefined , null , 0 , "" all are falsey values
+    Truthy values :  vice versa falsey values
+
+    */
+   /*
+
+    function calAge(par) {
+        return 2022 - par;
+    }
+
+    
+    
+    async function entering_value(){
+        var YN = prompt("Do you want to calculate age ? yes or no");
+        return YN;
+    }
+    
+    
+    var res = entering_value();
+    console.log(res);
+    
+ Write a JavaScript program to count the number of vowels in a given string
+ Write a JavaScript program to remove all characters from a given string that appear more than once
+}
+*/
+
+var inp = "This is just a simple string!";
+
+var arrr = inp.split("");
+var count = 0;
+arrr.forEach( (val,ind) => {
+
+    
+
+});
+console.log(count);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
